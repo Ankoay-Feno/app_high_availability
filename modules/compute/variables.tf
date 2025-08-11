9bfee564e408590223b73ac12b9ff6462a28ca89
ankoay feno <ankoayfeno@gmail.com>
2025-08-11 12:34:09 +0300

    Add variables for EC2 instance configuration in Terraform

---
+++ modules/compute/variables.tf
@@ -0,0 +1,24 @@
+variable "subnet_1" {
+  description = "Subnet 1 for EC2 instances"
+  type        = string  
+}
+
+variable "subnet_2" {
+  description = "Subnet 2 for EC2 instances"
+  type        = string  
+}
+
+variable "security_group" {
+  description = "Security group ID for EC2 instances"
+  type        = string
+}
+
+variable "key_path" {
+  description = "Path to the public key file for EC2 instances"
+  type        = string
+}
+
+variable "template_file" {
+  description = "Path to the Ansible inventory template file"
+  type        = string
+}
