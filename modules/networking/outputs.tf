45ec0d3a89ec01e9da153863aeff7f1039ad32ba
ankoay feno <ankoayfeno@gmail.com>
2025-08-11 12:34:44 +0300

    Add output definitions for VPC ID and public subnets in Terraform

---
+++ modules/networking/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+locals {
+  public_subnet_ids_json = { for k, subnet in aws_subnet.public_subnet: k => subnet.id }
+}
+
+output "subnet_1" {
+  value = local.public_subnet_ids_json["a"]
+}
+
+output "subnet_2" {
+  value = local.public_subnet_ids_json["b"]
+}
