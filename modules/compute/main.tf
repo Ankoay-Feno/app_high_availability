80934a020c8fc0fc2d849b58b927a2bded1e5eb9
ankoay feno <ankoayfeno@gmail.com>
2025-08-11 12:33:59 +0300

    Add main Terraform configuration for EC2 instances, EIP, and Ansible inventory

---
+++ modules/compute/main.tf
@@ -0,0 +1,59 @@
+resource "aws_key_pair" "key_pair" {
+  key_name   = "public-key"
+  public_key = file("${var.key_path}") 
+}
+
+locals {
+  ec2_instances = {
+    "instance_1" = { 
+      name = "EC2-Instance-1", 
+      subnet = var.subnet_1 
+    },
+    "instance_2" = { 
+      name = "EC2-Instance-2", 
+      subnet = var.subnet_2 
+    }
+  }
+}
+
+resource "aws_eip" "instance_eip" {
+  for_each = local.ec2_instances
+  tags = {
+    Name = each.value.name
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  for_each = local.ec2_instances
+
+  ami           = "ami-044415bb13eee2391"  
+  instance_type = "t2.nano"
+  key_name      = aws_key_pair.key_pair.key_name
+  subnet_id     = each.value.subnet
+  vpc_security_group_ids = [var.security_group]
+  
+  tags = {
+    Name = each.value.name
+  }
+}
+
+resource "aws_eip_association" "eip_association" {
+  for_each = aws_instance.ec2_instance
+
+  instance_id   = each.value.id
+  allocation_id = aws_eip.instance_eip[each.key].id
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "${path.root}/ansible/inventory.ini"
+  content = templatefile("${var.template_file}", {
+    instances = [
+      for k, instance in aws_instance.ec2_instance : {
+        name       = instance.tags.Name,
+        public_ip  = instance.public_ip,
+        ssh_user   = "ubuntu",
+        ssh_key_path    = "${abspath(".")}/keys/key"
+      }
+    ]
+  })
+}
